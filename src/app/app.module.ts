6301528b52b35b39f28f518f73b2a2fc5c12542e
Michael Schmid <schmid.m9@googlemail.com>
2023-02-27 10:16:12 +0100

    setup the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { CustomersComponent } from './customers/customers.component';
+import { AwardsComponent } from './awards/awards.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    ContactComponent,
+    RegisterComponent,
+    AboutComponent,
+    TestimonialsComponent,
+    CustomersComponent,
+    AwardsComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
